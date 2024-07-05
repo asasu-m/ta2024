966e73b8739a32016de500fe07e78e678d2b0af5
asasu-m <57764971+asasu-m@users.noreply.github.com>
2024-07-05 20:34:16 +0000

    説明文とクラスの問題追加

--- java/s_20240701/test/src/method_mondai.java
+++ java/s_20240701/test/src/method_mondai.java
@@ -17,14 +17,23 @@ public class method_mondai {
         //n1+n2を足してsumに代入してください。(addメソッドを使う。)
 
         System.out.println(n1+"+"+n2+"="+sum2);
-        //メソッドsubを作成して、3-1の計算をして、答えをprintlnで出力してください。
+        //メソッドsubを作成して、3-1(=2)の計算をして、答えをprintlnで出力してください。
+        /*
+         * 例：メソッドmultiの場合(掛け算)
+         * public static int multi(int n1,int n2)
+         * {
+         *      return n1*n2;
+         * }
+         * mainで呼び出すときは
+         * int n=multi(3,1);//nには3が代入される。
+         */
 
         return;
 
     }
     private static int add(int n1, int n2)
     {
-        //return文を作成してください。
+        //return文を作成してください。(TOI1は削除する(一時的に仮に書いたものです。))
         return TOI1;
     }
     private static void hello()
