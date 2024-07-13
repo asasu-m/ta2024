608d31feba8f86e1fcce6e80b9cf4fac0b94ef19
asasu-m <57764971+asasu-m@users.noreply.github.com>
2024-07-13 11:53:48 +0000

    問題場所修正

--- java/s_20240701/test/src/method_mondai.java
+++ java/s_20240701/test/src/method_mondai.java
@@ -12,12 +12,20 @@ public class method_mondai {
         System.out.println("2+3="+sum);
         //hello()を呼び出してください。
 
+        //この下の行にsubメソッドを呼び出す
+
+
         int n1=5,n2=3;
         int sum2=0;
         //n1+n2を足してsumに代入してください。(addメソッドを使う。)
 
         System.out.println(n1+"+"+n2+"="+sum2);
-        //メソッドsubを作成して、3-1(=2)の計算をして、答えをprintlnで出力してください。
+        
+
+        return;
+
+    }
+    //メソッドsubを作成して、3-1(=2)の計算をして、答えをprintlnで出力してください。
         /*
          * 例：メソッドmultiの場合(掛け算)
          * public static int multi(int n1,int n2)
@@ -27,10 +35,7 @@ public class method_mondai {
          * mainで呼び出すときは
          * int n=multi(3,1);//nには3が代入される。
          */
-
-        return;
-
-    }
+        
     private static int add(int n1, int n2)
     {
         //return文を作成してください。(TOI1は削除する(一時的に仮に書いたものです。))
